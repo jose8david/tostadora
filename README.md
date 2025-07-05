4dcf066cd0a2e7487571bdc0904118f6305c1445
jose8david <josedaviddiazcanton@gmail.com>
2025-07-05 11:52:17 +0200

    Proyecto para encender el LED de la placa de desarrollo mientras se mantiene pulsado el botón

---
+++ README.md
@@ -0,0 +1,12 @@
+# TostadoraButtonControl 🚀
+
+Este proyecto controla un relé mediante un botón físico en una placa Wemos D1 Mini (ESP8266). Incluye prevención de rebotes y está desarrollado en PlatformIO con control de versiones usando Git.
+
+## Características
+- Plataforma: PlatformIO + ESP8266
+- Lógica: C++ con debounce manual
+- Documentación: Markdown
+- Versionado: Git
+
+## Fase actual
+Encender el LED de la placa al pulsar un botón físico con control de rebote.
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,52 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// definición de los pines
+
+#define LED_PIN LED_BUILTIN
+#define BTN_PIN D2
+
+// Definición constante de tiempo debounce
+
+const unsigned long debounceDelay = 50; // 50 milisegundos
+
+// Definición de variables
+
+unsigned long lastDebounceTime = 0; 
+
+bool buttonState = HIGH;
+bool lastButtonReading = HIGH; 
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Comunicación serie
+  Serial.begin(115200);
+  // Configuración de los pines
+  pinMode(BTN_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  // Estado inicial del LED --> apagado
+  digitalWrite(LED_BUILTIN, LOW);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool currentButtonReading = digitalRead(BTN_PIN); // Lectura del botón con cada búcle
+
+  if (currentButtonReading != lastButtonReading) // Si el estado del botón cambia 
+  {
+    lastDebounceTime = millis(); // la variable lastDebounceTime toma t1 de pulsar el botón
+  }
+
+  if ((millis()- lastDebounceTime) > debounceDelay) // si el tiempo que se ha pulsado el botón es superior a 50 s.
+  {
+    if (currentButtonReading == LOW && buttonState == HIGH) // Se pulsa el botón (antes estaba sin pulsar)
+    {
+      digitalWrite(LED_PIN, HIGH);
+    }
+    
+    buttonState = currentButtonReading;
+    
+  }
+  
+  lastButtonReading = currentButtonReading;
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
